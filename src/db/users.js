177c995fbef376c132fea7bd8aac168386f1def0
YodaheaD <yosterdaniel@gmail.com>
2023-06-27 14:39:01 -0400

    1.1.1
    gitignore fix

--- src/db/users.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UsersTable = void 0;
-const mytablelike_1 = require("./mytablelike");
-exports.UsersTable = new mytablelike_1.myTableLike("users");
